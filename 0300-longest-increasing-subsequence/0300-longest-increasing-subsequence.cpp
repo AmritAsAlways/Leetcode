3feb67621160f9c47e16609e5959470580d28758
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-07-04 07:47:54 +0530

    0300-LONGEST-INCREASING-SUBSEQUENCE Stats: Time: 0 ms (100%), Space: 13.9 MB (94.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-//Memoization solution
-int solve(vector<int>&nums,int i,int prev,vector<vector<int>>&dp){
-    //base condition
-    int n=nums.size();
-    if(i==n) return 0;
-
-    if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-
-    if(prev==-1 || nums[prev]<nums[i]){
-        int take=1+solve(nums,i+1,i,dp);
-        int leave=solve(nums,i+1,prev,dp);
-        return dp[i][prev+1]=max(take,leave);
-    }
-    return dp[i][prev+1]=solve(nums,i+1,prev,dp);
-}
     int lengthOfLIS(vector<int>& nums) {
+        //binary search solution
         int n=nums.size();
-        //memoization 
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        // return solve(nums,0,-1,dp);
-
-        //tabulation 
-        vector<int>dp(n,-1);
-        int answer=INT_MIN;
+        vector<int>v;
         for(int i=0;i<n;i++){
-            dp[i]=1;
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]) dp[i]=max(dp[i],dp[j]+1);
+            auto it=lower_bound(v.begin(),v.end(),nums[i]);
+            int index=it-v.begin();
+
+            if(it==v.end()){
+                v.push_back(nums[i]);
+                continue;
             }
-            answer=max(answer,dp[i]);
+
+            v[index]=nums[i];
         }
-        return answer;
+        return v.size();
     }
 };
