35a8b9c6915d1f13b2fbe9bcf52bd97ca6feee45
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-07-10 10:38:09 +0530

    0206-REVERSE-LINKED-LIST Stats: Time: 0 ms (100%), Space: 13.4 MB (72.13%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *ahead=head,*curr=nullptr,*prev=nullptr;
+        while(ahead!=nullptr){
+            prev=curr;
+            curr=ahead;
+            ahead=ahead->next;
+            curr->next=prev;
+        }
+        return curr;
+    }
+};
--- README.md
+++ README.md
@@ -209,5 +209,10 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 ## Recursion
 |  |
 | ------- |
+| [0206-reverse-linked-list](https://github.com/AmritAsAlways/Leetcode/tree/master/0206-reverse-linked-list) |
 | [0233-number-of-digit-one](https://github.com/AmritAsAlways/Leetcode/tree/master/0233-number-of-digit-one) |
+## Linked List
+|  |
+| ------- |
+| [0206-reverse-linked-list](https://github.com/AmritAsAlways/Leetcode/tree/master/0206-reverse-linked-list) |
 <!---LeetCode Topics End-->
