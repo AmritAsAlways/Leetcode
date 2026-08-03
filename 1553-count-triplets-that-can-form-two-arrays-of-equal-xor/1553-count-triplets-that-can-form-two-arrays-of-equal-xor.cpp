5d9ae155ece1d98529c48bab1e7fb5299961a758
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-08-03 19:30:29 +0530

    1553-COUNT-TRIPLETS-THAT-CAN-FORM-TWO-ARRAYS-OF-EQUAL-XOR Stats: Time: 47 ms (7.67%), Space: 22 MB (8.66%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int countTriplets(vector<int>& arr) {
         int n=arr.size();
+        vector<int>totalxor(n,arr[0]);
+        for(int i=1;i<n;i++) totalxor[i]=totalxor[i-1]^arr[i];
         int answer=0;
-        for(int j=1;j<n;j++){
+        for(int i=1;i<n;i++){
             unordered_map<int,int>um;
-            int a=0;
-            for(int i=j-1;i>=0;i--){
-                a^=arr[i];
-                um[a]++;
+            //all the possible xor values from index 0 to j-1
+            for(int j=0;j<i;j++){
+                if(j==0) um[totalxor[i-1]]++;
+                else um[totalxor[i-1]^totalxor[j-1]]++;
             }
-            int b=0;
-            for(int k=j;k<n;k++){
-                b^=arr[k];
-                if(um.count(b)) answer+=um[b];
+            //all the possible xor values from index j to k
+            for(int k=i;k<n;k++){
+                int xorvalue=totalxor[k]^totalxor[i-1];
+                if(um.count(xorvalue)) answer+=um[xorvalue];
             }
+            cout<<answer<<endl;
         }
         return answer;
     }
