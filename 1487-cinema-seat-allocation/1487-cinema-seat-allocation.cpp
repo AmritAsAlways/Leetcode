a2a6c7d2b72564b130d27c225ae0b1087cec5bec
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-08-19 19:43:19 +0530

    1487-CINEMA-SEAT-ALLOCATION Stats: Time: 31 ms (61.66%), Space: 60.7 MB (58.61%) - LeetHub

--- 1487-cinema-seat-allocation/1487-cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/1487-cinema-seat-allocation.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        int m = reservedSeats.size(), answer = 0;
-        sort(reservedSeats.begin(), reservedSeats.end(),
-             [](vector<int>& a, vector<int>& b) {
-                 if (a[0] == b[0])
-                     return a[1] > b[1];
-                 return a[0] < b[0];
-             });
+        int m = reservedSeats.size(),answer = 0;
+        sort(reservedSeats.begin(), reservedSeats.end(),[](vector<int>& a,vector<int>&b){
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
         int prevanswer = 2 * (reservedSeats[0][0] - 1);
         int i=0;
         while (i < m) {
@@ -23,13 +21,11 @@ public:
 
             if (v[5] - v[1] == 0) {
                 answer++;
-                if (v[9] - v[5] == 0)
-                    answer++;
-            } else if (v[7] - v[3] == 0) {
-                answer++;
-            } else if (v[9] - v[5] == 0) {
-                answer++;
-            }
+                if (v[9] - v[5] == 0) answer++;
+            } 
+            else if (v[7] - v[3] == 0) answer++;
+            else if (v[9] - v[5] == 0) answer++;
+            
             int lastrow=n,firstrow=reservedSeats[i-1][0]+1;
             if(i!=m) lastrow=reservedSeats[i][0]-1;
             int intervalanswer=2*(lastrow-firstrow+1);
