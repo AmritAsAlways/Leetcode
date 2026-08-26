655683ef786c39b8f886ff1ebc64c0b1f85dbf20
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-08-26 15:12:19 +0530

    1386-SHIFT-2D-GRID Stats: Time: 0 ms (100%), Space: 18.2 MB (36.53%) - LeetHub

--- 1386-shift-2d-grid/1386-shift-2d-grid.cpp
+++ 1386-shift-2d-grid/1386-shift-2d-grid.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>ans(m,vector<int>(n,0));
-        int rowahead=(k/n)%m,colahead=k%n;
+        int m=grid.size(),n=grid[0].size(),totaloperation=m*n;
+        k%=totaloperation;
+        int rowahead=k/n,colahead=(k%n);
+        cout<<rowahead<<" "<<colahead<<endl;
+        vector<vector<int>>v(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                cout<<rowahead<<" "<<colahead<<endl;
-                ans[rowahead][colahead]=grid[i][j];
-                colahead++;
-                if(colahead==n){
-                    colahead=0;
-                    rowahead=(rowahead+1)%m;
-                }
+                int col=j+colahead,row=i+rowahead;
+                if(col>=n) row++;
+                v[row%m][col%n]=grid[i][j];
             }
         }
-        return ans;
+        return v;
     }
 };
