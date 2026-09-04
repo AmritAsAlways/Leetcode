eb27db0530445f007a7c1e17356ce51fb5b86bad
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-04 15:55:38 +0530

    4284-SMALLEST-STABLE-INDEX-I Stats: Time: 0 ms (100%), Space: 31.1 MB (66.39%) - LeetHub

--- 4284-smallest-stable-index-i/4284-smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/4284-smallest-stable-index-i.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
+        // int n=nums.size();
+        // multiset<int>s;
+        // for(int i=0;i<n;i++) s.insert(nums[i]);
+        // int maxi=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     maxi=max(maxi,nums[i]);
+        //     int mini=*s.begin();
+
+        //     if(maxi-mini<=k) return i;
+
+        //     auto it=s.find(nums[i]);
+        //     s.erase(it);
+        // }
+        // return -1;
+
+        //other method
         int n=nums.size();
-        multiset<int>s;
-        for(int i=0;i<n;i++) s.insert(nums[i]);
+        vector<int>mini(n,INT_MAX);
+        for(int i=n-1;i>=0;i--){
+            mini[i]=nums[i];
+            if(i!=n-1) mini[i]=min(mini[i],mini[i+1]);
+        }
         int maxi=INT_MIN;
         for(int i=0;i<n;i++){
             maxi=max(maxi,nums[i]);
-            int mini=*s.begin();
-
-            if(maxi-mini<=k) return i;
-
-            auto it=s.find(nums[i]);
-            s.erase(it);
+            if(maxi-mini[i]<=k) return i;
         }
         return -1;
     }
