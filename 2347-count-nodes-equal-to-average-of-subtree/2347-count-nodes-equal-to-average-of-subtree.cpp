ac63467a1acb06836dfdd1c2d07112c2d3733e78
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-15 04:36:55 +0530

    2347-COUNT-NODES-EQUAL-TO-AVERAGE-OF-SUBTREE Stats: Time: 0 ms (100%), Space: 15.9 MB (29.12%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,17 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    int ans = 0;
-    pair<int,int> dfs(TreeNode* node) {
-        if (!node) return {0, 0};
-        auto [ls, lc] = dfs(node->left);
-        auto [rs, rc] = dfs(node->right);
-        int sum = ls + rs + node->val;
-        int cnt = lc + rc + 1;
-        if (sum / cnt == node->val) ans++;
-        return {sum, cnt};
-    }
 public:
+pair<int,int> inorder(TreeNode* root,int&answer){
+    if(root==nullptr) return {0,0};
+
+    pair<int,int> x=inorder(root->left,answer);
+    pair<int,int> y=inorder(root->right,answer);
+
+    int sum=root->val+x.first+y.first,size=1+x.second+y.second;
+    int avg=sum/size;
+
+    if(root->val==avg) answer++;
+
+    return {sum,size};
+}
     int averageOfSubtree(TreeNode* root) {
-        dfs(root);
-        return ans;
+        int answer=0;
+        inorder(root,answer);
+        return answer;
     }
 };
