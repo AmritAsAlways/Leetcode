7ff2488c3bc42b6d11e0bfc5e33a2daf65360058
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-17 02:50:40 +0530

    1798-MAX-NUMBER-OF-K-SUM-PAIRS Stats: Time: 85 ms (78.33%), Space: 62.1 MB (47.73%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_map<int,int>um;
-        for(int i=0;i<n;i++){
-            if(nums[i]>k) continue;
-            um[nums[i]]++;
-        }
-        int answer=0;
-        while(!um.empty()){
-            auto it=um.begin();
-            int num1=it->first,times1=it->second;
+        int n=nums.size(),answer=0;
+        sort(nums.begin(),nums.end());
+        int i=0,j=n-1;
+        while(i<j){
+            int sum=nums[i]+nums[j];
 
-            if((double)num1==(double)k/2){
-                answer+=times1/2;
-                um.erase(num1);
-                continue;
+            if(sum==k){
+                i++;
+                j--;
+                answer++;
             }
-
-            if(um.find(k-num1)==um.end()){
-                um.erase(num1);
-                continue;
+            else if(sum>k){
+                j--;
             }
-
-            int times2=um[k-num1];
-            answer+=min(times1,times2);
-            um.erase(num1);
-            um.erase(k-num1);
+            else{
+                i++;
+            }
         }
         return answer;
     }
