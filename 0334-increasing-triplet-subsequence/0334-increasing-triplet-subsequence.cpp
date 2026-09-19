6f3f6e1e8b6956b9f6abd6dcaef5495db9d54cac
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-19 11:06:15 +0530

    0334-INCREASING-TRIPLET-SUBSEQUENCE Stats: Time: 7 ms (13.18%), Space: 125.2 MB (5.6%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -10,9 +10,8 @@ public:
         for(int i=n-1;i>=0;i--){
             if(i==n-1) continue;
             rightmaxi[i]=max(rightmaxi[i+1],nums[i+1]);
-        }
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>leftmini[i] && rightmaxi[i]>nums[i]) return true;
+
+            if(i!=0 && i!=n-1 && nums[i]>leftmini[i] && rightmaxi[i]>nums[i]) return true; 
         }
         return false;
     }
