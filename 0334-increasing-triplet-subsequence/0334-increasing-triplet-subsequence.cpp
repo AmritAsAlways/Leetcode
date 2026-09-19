c9e022fade5fe293e73a3d3f4454b23bc9f49f21
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-19 11:10:47 +0530

    0334-INCREASING-TRIPLET-SUBSEQUENCE Stats: Time: 4 ms (24.55%), Space: 115.8 MB (34.47%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>rightmaxi(n,INT_MIN),leftmini(n,INT_MAX);
-        for(int i=0;i<n;i++){
-            if(i==0) continue;
-            leftmini[i]=min(leftmini[i-1],nums[i-1]);
-        }
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1) continue;
-            rightmaxi[i]=max(rightmaxi[i+1],nums[i+1]);
+        // int n=nums.size();
+        // vector<int>rightmaxi(n,INT_MIN),leftmini(n,INT_MAX);
+        // for(int i=0;i<n;i++){
+        //     if(i==0) continue;
+        //     leftmini[i]=min(leftmini[i-1],nums[i-1]);
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     if(i==n-1) continue;
+        //     rightmaxi[i]=max(rightmaxi[i+1],nums[i+1]);
 
-            if(i!=0 && i!=n-1 && nums[i]>leftmini[i] && rightmaxi[i]>nums[i]) return true; 
+        //     if(i!=0 && i!=n-1 && nums[i]>leftmini[i] && rightmaxi[i]>nums[i]) return true; 
+        // }
+        // return false;
+
+        //best solution
+        int n=nums.size();
+        int first=INT_MAX,second=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(nums[i]<=first){
+                first=nums[i];
+            }
+            else if(nums[i]<=second){
+                second=nums[i];
+            }
+            else return true;
         }
         return false;
     }
