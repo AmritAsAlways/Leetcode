9931cd16e987f7a7a53ce007c4fcbd3a62800351
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-23 19:13:42 +0530

    1258-ARTICLE-VIEWS-I Stats: Time: 610 ms (10.66%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select distinct author_id as id from Views as v where v.author_id=v.viewer_id order by id asc
+with temporarytable as (
+    select author_id as id from Views as v where v.author_id=v.viewer_id
+)
+
+select distinct id from temporarytable order by id asc
