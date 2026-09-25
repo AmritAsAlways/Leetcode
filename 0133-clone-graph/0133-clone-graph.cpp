0c34a3ec1b501141d52afebd8e2c094c4656444f
Amrit Raj Singh <amritrajsingh300@gmail.com>
2026-09-25 23:12:16 +0530

    0133-CLONE-GRAPH Stats: Time: 3 ms (67.99%), Space: 12.5 MB (5.24%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -25,14 +25,14 @@ Node* solve(Node* node,unordered_map<Node*,Node*>&um){
     if(node==nullptr) return nullptr;
     Node* copy=new Node();
     copy->val=node->val;
-    um[node]=copy;
+    um[node]=copy; //save the copy and the node in a single unit so we can use it
     vector<Node*>v;
     for(int i=0;i<node->neighbors.size();i++){
         if(um.find(node->neighbors[i])==um.end()){
             v.push_back(solve(node->neighbors[i],um));
         }
         else{
-            v.push_back(um[node->neighbors[i]]);
+            v.push_back(um[node->neighbors[i]]); //here we donot have to create a another node copy that already exits we have to just use the same copy already created by us before and saved 
         }
     }
     copy->neighbors=v;
